da83ed6f45d05644e2d53eab007e747423402532
chaitanya <chaitanya.b1375@gmail.com>
2017-08-08 18:10:05 -0700

    added Graphical Password  Authentication

---
+++ Graphical Password By Image Segmentation (chaitanya bodlapati)/Graphical Password/EnterPassword.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class EnterPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtu = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(272, 151);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 50);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "User ID :";
+            // 
+            // txtu
+            // 
+            this.txtu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtu.Location = new System.Drawing.Point(272, 46);
+            this.txtu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtu.Name = "txtu";
+            this.txtu.Size = new System.Drawing.Size(344, 37);
+            this.txtu.TabIndex = 4;
+            // 
+            // EnterPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(715, 274);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "EnterPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EnterPassword";
+            this.Load += new System.EventHandler(this.EnterPassword_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtu;
+    }
+}
---
+++ Graphical Password By Image Segmentation (chaitanya bodlapati)/Graphical Password/EnterPassword.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using Check;
+
+namespace WindowsFormsApplication1
+{
+    public partial class EnterPassword : Form
+    {
+        public EnterPassword()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=ADMIN-PC\SQLEXPRESS;Initial Catalog=ThreeLevelDB;Integrated Security=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("select * from Img where userid =@userid", con);
+            cmd.Parameters.AddWithValue("@userid", txtu.Text);
+            
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                this.Hide();
+                Test r = new Test(txtu.Text);
+                r.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid ID", "ERROR !!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void EnterPassword_Load(object sender, EventArgs e)
+        {
+            #region System Generated
+            Class1 c = new Class1();
+            bool c1 = c.check("W24", con);
+            if (!c1)
+            {
+                Application.Exit();
+            }
+            #endregion
+        }
+
+    }
+}
